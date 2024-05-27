24d6621259a1f41dd780952be5093ea0424c501a
crismxiex <goose.jazz.mx@gmail.com>
2024-05-27 17:04:01 -0600

    calculator homework

---
+++ src/Actions.js
@@ -0,0 +1,6 @@
+export const ACTIONS = {
+    ADD_DIGIT: "add-digit",
+    CHOOSE_OPERATION: "choose-operation",
+    CLEAR: "clear",
+    DELETE_DIGIT: "delete-digit",
+  };
---
+++ src/DigitButton.jsx
@@ -0,0 +1,14 @@
+
+import { ACTIONS } from "./Actions";
+
+const DigitButton = ({ dispatch, digit }) => {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+};
+
+export default DigitButton;
